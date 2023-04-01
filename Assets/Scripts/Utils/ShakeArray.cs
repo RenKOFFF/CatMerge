27f85233ef32e13ece07b7c9f73352ecb5c87ece
re_nKOFF <lex.keivi@mail.ru>
2023-04-01 14:33:18 +0500

    added utils (shake array)

---
+++ Assets/Scripts/Utils/ShakeArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class ShakeArray<T>
+    {
+        public static T[] Shake(T[] array)
+        {
+            var random = new Random();
+
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+                (array[j], array[i]) = (array[i], array[j]);
+            }
+
+            return array;
+        }
+    }
+}
