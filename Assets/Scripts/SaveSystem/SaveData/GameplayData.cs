6d37aff742be5d80bdfe9e708c8f73d761453e3c
Alexey Kucherenko <82213188+RenKOFFF@users.noreply.github.com>
2023-04-30 12:46:44 +0500

    Splited save data files

---
+++ Assets/Scripts/SaveSystem/SaveData/GameplayData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using GameData;
+using Newtonsoft.Json;
+
+namespace SaveSystem
+{
+    [Serializable]
+    public class GameplayData
+    {
+        public int CurrentEnergy;
+        public int Money;
+        public string OpenedLevelsDictionaryJSonFormat;
+        public int CurrentLevel;
+
+        public GameplayData(GameManager gameManager)
+        {
+            CurrentEnergy = gameManager.Energy;
+            Money = gameManager.Money;
+
+            var openedLevelsDictionary = gameManager.OpenedLevels;
+            OpenedLevelsDictionaryJSonFormat = JsonConvert.SerializeObject(openedLevelsDictionary);
+
+            CurrentLevel = gameManager.CurrentLevel;
+        }
+
+        /// <summary>
+        /// Default values
+        /// </summary>
+        public GameplayData()
+        {
+            CurrentEnergy = GameManager.EnergyController.MaxStartEnergy;
+            Money = 0;
+
+            var startDict = new Dictionary<int, bool>();
+            startDict.Add(1, true);
+            OpenedLevelsDictionaryJSonFormat = JsonConvert.SerializeObject(startDict);
+
+            CurrentLevel = 0;
+        }
+    }
+}
--- Assets/Scripts/SaveSystem/SaveData/LevelSaveData.cs
+++ Assets/Scripts/SaveSystem/SaveData/LevelSaveData.cs
@@ -1,47 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using GameData;
 using Merge;
 using Merge.Energy;
 using Newtonsoft.Json;
 
 namespace SaveSystem
 {
-    [Serializable]
-    public class GameplayData
-    {
-        public int CurrentEnergy;
-        public int Money;
-        public string OpenedLevelsDictionaryJSonFormat;
-        public int CurrentLevel;
-
-        public GameplayData(GameManager gameManager)
-        {
-            CurrentEnergy = gameManager.Energy;
-            Money = gameManager.Money;
-
-            var openedLevelsDictionary = gameManager.OpenedLevels;
-            OpenedLevelsDictionaryJSonFormat = JsonConvert.SerializeObject(openedLevelsDictionary);
-
-            CurrentLevel = gameManager.CurrentLevel;
-        }
-
-        /// <summary>
-        /// Default values
-        /// </summary>
-        public GameplayData()
-        {
-            CurrentEnergy = GameManager.EnergyController.MaxStartEnergy;
-            Money = 0;
-
-            var startDict = new Dictionary<int, bool>();
-            startDict.Add(0, true);
-            OpenedLevelsDictionaryJSonFormat = JsonConvert.SerializeObject(startDict);
-
-            CurrentLevel = 0;
-        }
-    }
-
     [Serializable]
     public class LevelSaveData
     {
