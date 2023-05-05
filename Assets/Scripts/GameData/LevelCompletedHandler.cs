f9415089f38e899ae7af4565bc348f6e71b4e6ca
Alexey Koposov <cmango67@gmail.com>
2023-05-05 22:37:46 +0500

    Add win screen

---
+++ Assets/Scripts/GameData/LevelCompletedHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GameData
+{
+    public class LevelCompletedHandler : MonoBehaviour
+    {
+        public void OpenMenu()
+        {
+            var canvas =  GameObject.FindGameObjectWithTag(GameConstants.Tags.Canvas);
+            var menuCanvas =  GameObject.FindGameObjectWithTag(GameConstants.Tags.MenuCanvas);
+
+            canvas.SetActive(false);
+            menuCanvas.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
