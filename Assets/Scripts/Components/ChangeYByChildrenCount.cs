4eb7780fd976322450d2219a99d7a06d832c65d0
Alexey Koposov <cmango67@gmail.com>
2023-05-24 23:54:10 +0500

    Add dynamic height in tree panel

---
+++ Assets/Scripts/Components/ChangeYByChildrenCount.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class ChangeYByChildrenCount : MonoBehaviour
+    {
+        [SerializeField] private float firstY;
+        [SerializeField] private float secondY;
+        [SerializeField] private int changeWhenChildrenCountMoreThan;
+
+        private RectTransform _rectTransform;
+
+        private void UpdateY()
+        {
+            var y = transform.childCount > changeWhenChildrenCountMoreThan
+                ? secondY
+                : firstY;
+
+            _rectTransform.anchoredPosition = new Vector2(_rectTransform.anchoredPosition.x, y);
+        }
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Update()
+        {
+            UpdateY();
+        }
+    }
+}
---
+++ Assets/Scripts/Components/ControlHeightByChildrenSize.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Components
+{
+    public class ControlHeightByChildrenSize : MonoBehaviour
+    {
+        [SerializeField] private List<RectTransform> children;
+        [SerializeField] private float offset;
+
+        private RectTransform _rectTransform;
+
+        private void UpdateHeight()
+        {
+            var sumHeight = children.Sum(child => child.rect.height) + offset;
+            _rectTransform.sizeDelta = new Vector2(_rectTransform.rect.width, sumHeight);
+        }
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Update()
+        {
+            UpdateHeight();
+        }
+    }
+}
