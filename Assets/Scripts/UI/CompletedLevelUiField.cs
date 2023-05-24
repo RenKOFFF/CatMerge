06c658b42d8f988375118a7d086c859e6ebcdf43
Alexey Kucherenko <82213188+RenKOFFF@users.noreply.github.com>
2023-05-25 00:47:28 +0500

    level ui field

---
+++ Assets/Scripts/UI/CompletedLevelUiField.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameData;
+using Orders;
+
+namespace UI
+{
+    public class CompletedLevelUiField: CurrencyFillElement
+    {
+        private int _maxLevelInZone = 5;
+        private int _openedLevels;
+
+        private float _coeff;
+        private void Start()
+        {
+            //TODO: это полное говнище, исправить, если проект будет не заброшен
+            _openedLevels = GameManager.Instance.OpenedLevels.Values.Where(a => true).ToList().Count;
+
+            _coeff = 100f / _maxLevelInZone;
+
+            Initialize(_maxLevelInZone * _coeff, (_openedLevels - 1) * _coeff);
+            _currencyText.text += "%";
+            
+            OrderManager.Instance.LevelCompleted += OnCompletedOrdersChanged;
+        }
+
+        private void OnDestroy()
+        {
+            OrderManager.Instance.LevelCompleted -= OnCompletedOrdersChanged;
+        }
+
+        private void OnCompletedOrdersChanged()
+        {
+            ChangeValue(_coeff * _openedLevels++);
+            _currencyText.text += "%";
+        }
+    }
+}
