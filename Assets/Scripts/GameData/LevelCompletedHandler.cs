f020fca7e0b07553cee197492627014e0fc56c15
Alexey Kucherenko <82213188+RenKOFFF@users.noreply.github.com>
2023-06-05 15:01:53 +0500

    Level complited anims

--- Assets/Scripts/GameData/LevelCompletedHandler.cs
+++ Assets/Scripts/GameData/LevelCompletedHandler.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,7 +10,30 @@ namespace GameData
 {
     public class LevelCompletedHandler : MonoBehaviour
     {
+        [SerializeField] private GameObject _cat;
+
         private UnityAction OnOpenMenu;
+        private TweenerCore<Quaternion, Vector3, QuaternionOptions> _tween;
+
+        private void Start()
+        {
+            transform.localScale = Vector3.zero;
+
+            float durationPanel = 0.8f;
+            transform
+                .DOScale(Vector3.one, durationPanel)
+                .OnComplete(() => PlayCatAnimation());
+        }
+
+        private void PlayCatAnimation()
+        {
+            float durationCat = 2f;
+
+            _tween = _cat.transform
+                .DOLocalRotate(new Vector3(0, 0, 45), durationCat)
+                .SetEase(Ease.InOutQuint)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
 
         public void Initialize(UnityAction onOpenMenu)
         {
@@ -16,6 +42,7 @@ namespace GameData
 
         public void OpenMenu()
         {
+            _tween.Kill();
             OnOpenMenu();
         }
     }
