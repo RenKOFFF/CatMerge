cf1de84e2e6d9c156db84404e2bc166a402240c9
Alexey Kucherenko <82213188+RenKOFFF@users.noreply.github.com>
2023-06-12 13:47:47 +0500

    added sprites + levelbutton prefab

---
+++ Assets/Scripts/MainMenu/MainMenuProgressPanel.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace MainMenu
+{
+    public class MainMenuProgressPanel : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        public void ShowProgressByLevelIndex(int instanceCurrentLevel)
+        {
+            //_text.text = 
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu/UiLevelButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GameData;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class UiLevelButton : MonoBehaviour
+    {
+        [SerializeField] private int _levelIndex;
+        
+        [SerializeField] private Sprite _normalSprite;
+        [SerializeField] private Sprite _selectedSprite;
+        [SerializeField] private Sprite _completedSprite;
+        
+        [SerializeField] private Image _image;
+        
+        [SerializeField] private MainMenuProgressPanel _progressPanel;
+        [SerializeField] private GameObject _halo;
+
+
+        public void ShowProgress()
+        {
+            _progressPanel.gameObject.SetActive(true);
+            _progressPanel.ShowProgressByLevelIndex(GameManager.Instance.CurrentLevel);
+            
+            _image.sprite = _selectedSprite;
+        }
+    }
+}
