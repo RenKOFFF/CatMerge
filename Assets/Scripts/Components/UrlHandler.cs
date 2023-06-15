f16f90d7f63e859323a6907eb18d5bb4ad11f553
Alexey Koposov <cmango67@gmail.com>
2023-06-15 17:48:11 +0500

    Add description in settings

---
+++ Assets/Scripts/Components/UrlHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class UrlHandler : MonoBehaviour
+    {
+        [SerializeField] private string url = "https://youtu.be/dQw4w9WgXcQ";
+
+        public void OpenUrl()
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
