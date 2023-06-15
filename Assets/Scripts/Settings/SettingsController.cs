056ce5d5629b84d16844adce871f596993fb68a8
Alexey Koposov <cmango67@gmail.com>
2023-06-15 18:43:35 +0500

    Add mock music buttons

--- Assets/Scripts/Settings/SettingsController.cs
+++ Assets/Scripts/Settings/SettingsController.cs
@@ -9,19 +9,30 @@ namespace Settings
         [SerializeField] private GameObject[] _buttons;
         [SerializeField] private GameObject _panel;
         [SerializeField] private Image _bg;
-        
+
         [SerializeField] private Color _openedColor;
         [SerializeField] private Color _closedColor;
-        
+
+        [SerializeField] private Image musicButton;
+        [SerializeField] private Sprite musicIsOnSprite;
+        [SerializeField] private Sprite musicIsOffSprite;
+
+        [SerializeField] private Image soundsButton;
+        [SerializeField] private Sprite soundsIsOnSprite;
+        [SerializeField] private Sprite soundsIsOffSprite;
+
         private bool _animationInProcess;
+
+        private static bool _isMusicOn = true;
+        private static bool _isSoundsOn = true;
 
         private void Start()
         {
             _panel.transform.localScale = Vector3.zero;
-            
+
             _bg.color = _closedColor;
             _bg.gameObject.SetActive(false);
-            
+
             foreach (var button in _buttons)
             {
                 button.transform.localScale = Vector3.zero;
@@ -33,6 +44,8 @@ namespace Settings
             if (_animationInProcess) return;
 
             _animationInProcess = true;
+            UpdateMusicSprite();
+            UpdateSoundsSprite();
 
             var seq = DOTween.Sequence();
 
@@ -50,7 +63,7 @@ namespace Settings
                         .DOScale(isOpening ? Vector3.one : Vector3.zero, buttonDuration)
                         .SetEase(isOpening ? Ease.OutBack : Ease.InBack));
             }
-            
+
             _bg.gameObject.SetActive(true);
             seq.Insert(0, _bg.DOColor(isOpening ? _openedColor : _closedColor, durationPanel));
 
@@ -59,6 +72,28 @@ namespace Settings
                 _animationInProcess = false;
                 if (!isOpening) _bg.gameObject.SetActive(false);
             });
+        }
+
+        public void SwitchMusic()
+        {
+            _isMusicOn = !_isMusicOn;
+            UpdateMusicSprite();
+        }
+
+        private void UpdateMusicSprite()
+        {
+            musicButton.sprite = _isMusicOn ? musicIsOnSprite : musicIsOffSprite;
+        }
+
+        public void SwitchSounds()
+        {
+            _isSoundsOn = !_isSoundsOn;
+            UpdateSoundsSprite();
+        }
+
+        private void UpdateSoundsSprite()
+        {
+            soundsButton.sprite = _isSoundsOn ? soundsIsOnSprite : soundsIsOffSprite;
         }
     }
 }
